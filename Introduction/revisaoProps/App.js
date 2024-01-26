ee067179878db303c3f0966827f07ab84c174375
Gustavo Magalhães Do Nascimento <53844614893@info.local>
2024-01-26 17:19:13 -0300

    Introduction - 26/01/2024

--- Introduction/revisaoProps/App.js
+++ Introduction/revisaoProps/App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Person from './src/components/Person/Person'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+      <Person name='Gustavo' age={18}/>
+      <Person name='Gustavo' age={18}/>
+      <Person name='Gustavo' age={18}/>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+      
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
