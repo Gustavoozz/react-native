9442b078469575f32cbe788ec19201859253c096
Gustavo Magalhães Do Nascimento <53844614893@info.local>
2024-02-05 16:00:21 -0300

    Aula 05/02 - Consumo da API

--- Introduction/viaCep/service/Services.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'react-native-axios';
-
-//  const externalApiUri = `https://viacep.com.br/ws/`
-
-//  const api = axios.Create({
-//     baseUrl: externalApiUri
-// })
-
-//  export default api;
