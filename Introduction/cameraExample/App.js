fa136711f68babe19e94d9728494091e09495dc8
Gustavo Magalhães Do Nascimento <53844614893@info.local>
2024-03-14 15:48:48 -0300

    AUla 14/03

--- Introduction/cameraExample/App.js
+++ Introduction/cameraExample/App.js
@@ -3,6 +3,7 @@ import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-na
 import { Camera, CameraType } from 'expo-camera';
 import { useEffect, useState, useRef } from 'react';
 import { FontAwesome } from '@expo/vector-icons'
+import * as MediaLibrary from 'expo-media-library'
 
 export default function App() {
   const cameraRef = useRef(null);
@@ -13,6 +14,8 @@ export default function App() {
   useEffect(() => {
     ( async () => {
       const { status : cameraStatus } = await Camera.requestCameraPermissionsAsync();
+
+      const { status : mediaStatus } = await MediaLibrary.requestPermissionsAsync();
     })();
   }, [])
 
@@ -25,6 +28,21 @@ export default function App() {
 
       console.log(photo)
     }
+  }
+
+  function ClearPhoto() {
+    setPhoto(null);
+
+    setOpenModal(false);
+  }
+
+  async function UploadPhoto() {
+    await MediaLibrary.createAssetAsync(photo)
+    .then(() => {
+      alert("Foto salva com sucesso!")
+    }).catch( error => {
+      alert("Não foi possível processar a foto!")
+    })
   }
 
   return (
@@ -49,8 +67,17 @@ export default function App() {
       <Modal animationType='slide' transparent={false} visible={openModal}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 20 }}>
 
-      <View style ={{ margin: 10, flexDirection: 'row' }}>
+      <View style ={{ margin: 10, flexDirection: 'row', gap: 20 }}>
 
+      <TouchableOpacity style={ styles.btnClear } onPress={() => ClearPhoto()}>
+      <FontAwesome name='trash' size={35} color='#121212'/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={ styles.btnUpload } onPress={() => UploadPhoto()}>
+      <FontAwesome name='upload' size={35} color='#121212`'/>
+      </TouchableOpacity>
+
+      
       </View>
 
       <Image
@@ -100,6 +127,22 @@ const styles = StyleSheet.create({
     margin: 20,
     borderRadius: 10,
     backgroundColor: '#121212',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btnClear: {
+    padding: 20,
+    backgroundColor: 'transparent',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btnUpload: {
+    padding: 20,
+    backgroundColor: 'transparent',
 
     justifyContent: 'center',
     alignItems: 'center',
